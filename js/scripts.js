e9c0041833175b98a6ac710dc9db0645bc2f4916
Mittal-Malankiya <m4mittal19@gmail.com>
2024-01-05 12:32:21 +0100

    added list of array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-let pokemonList = []; 
+let pokemonList = [
+{ name: 'Bulbasaur', type: 'Grass/Poison', level: 7 },
+{ name: 'Charmander', type: 'Fire', level: 7 },
+{ name: 'Squirtle', type: 'Water', level: 8 },
+]; 

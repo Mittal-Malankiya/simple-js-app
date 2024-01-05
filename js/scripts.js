31cb20dba0ecbe926ccd2e4318b34e634fa16c9b
Mittal-Malankiya <m4mittal19@gmail.com>
2024-01-05 12:43:15 +0100

    changed list type

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-{ name: 'Bulbasaur', type: 'Grass/Poison', level: 7 },
+{ name: 'Bulbasaur', type: ['grass', 'poison'], level: 7 },
 { name: 'Charmander', type: 'Fire', level: 7 },
 { name: 'Squirtle', type: 'Water', level: 8 },
 ]; 

86dd19c1940ce5916dbd1a783aa41d74be01fc42
Mittal-Malankiya <m4mittal19@gmail.com>
2024-01-06 17:11:24 +0100

    added for loop

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,6 @@ let pokemonList = [
 { name: 'Charmander', type: ['Fire'], level: 7 },
 { name: 'Squirtle', type: ['Water'], level: 8 },
 ]; 
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].level})</p>`);
+  }

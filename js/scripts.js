093b295a2aaaddfce9a54b1c5409cd1b7f8fc57a
Mittal-Malankiya <m4mittal19@gmail.com>
2024-01-06 17:50:00 +0100

    added again code

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 let pokemonList = [
-{ name: 'Bulbasaur', type: ['grass', 'poison'], level: 7 },
-{ name: 'Charmander', type: ['Fire'], level: 7 },
-{ name: 'Squirtle', type: ['Water'], level: 8 },
-]; 
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].level})</p>`);
+    { name: 'Bulbasaur', type: ['grass', 'poison'], level: 7 },
+    { name: 'Charmander', type: ['Fire'], level: 7 },
+    { name: 'Squirtle', type: ['Water'], level: 8 },
+  ];
+  
+  for (let i = 0; i < pokemonList.length; i++) {
+    document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].level})`);
+  
+    if (pokemonList[i].level > 7) {
+      document.write(' - Wow, that\'s big!');
+    }
+  
+    document.write('</p>');
   }

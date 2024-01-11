a30a3d9bf2785be4693b0f403e3c31df536b8b40
Mittal-Malankiya <m4mittal19@gmail.com>
2024-01-11 08:12:01 +0100

    wraped with IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-let pokemonList = [
+(function() {
+  let pokemonList = [
     { name: 'Bulbasaur', type: ['grass', 'poison'], level: 7 },
     { name: 'Charmander', type: ['Fire'], level: 7 },
     { name: 'Squirtle', type: ['Water'], level: 8 },
   ];
-  
   /*for (let i = 0; i < pokemonList.length; i++) {
     document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].level})`);
   
@@ -20,3 +20,4 @@ let pokemonList = [
     console.log(`Level: ${pokemon.level}`);
     console.log('  ');
   });
+})();

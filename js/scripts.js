0aa604d2a6392c8f04fb039d5c44f02f9a82ade3
Mittal-Malankiya <m4mittal19@gmail.com>
2024-01-14 14:50:59 +0100

    added js

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,32 @@
-let pokemonRepository = (function() {
-  let pokemonList = [
+
+  let pokemonRepository =(function () {
+    function addListItem(pokemon) {
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        
+        button.innerText = pokemon.name;
+        button.classList.add('custom-button');
+        button.addEventListener('click', function () {
+          showDetails(pokemon);
+      });
+        listItem.appendChild(button);
+        document.querySelector('.pokemon-list').appendChild(listItem);
+    }
+    function showDetails(pokemon) {
+      console.log('Pokemon Details:', pokemon);
+    }
+   let pokemonArray = [
     { name: 'Bulbasaur', type: ['grass', 'poison'], level: 7 },
     { name: 'Charmander', type: ['Fire'], level: 7 },
-    { name: 'Squirtle', type: ['Water'], level: 8 },
-  ];
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
-
-  function getAll() {
-    return pokemonList;
-  }
-
-  return {
-    add: add,
-    getAll: getAll
-  };
+    { name: 'Squirtle', type: ['Water'], level: 8 },];
+  
+    pokemonArray.forEach(pokemon => {
+        addListItem(pokemon);
+    });
+    return {
+        addListItem: addListItem,
+        showDetails:showDetails
+    };
   })();
-  pokemonRepository.getAll().forEach(function(pokemon, index) {
-    console.log(`
-    Pokemon at index ${index}:
-    Name: ${pokemon.name}
-    Type: ${pokemon.type}
-    Level: ${pokemon.level}
-    `);
-  });
 
 

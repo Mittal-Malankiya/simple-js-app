c8bfff50f8c98b0cba1aae5c971ebf110f5f07a4
Mittal-Malankiya <m4mittal19@gmail.com>
2024-01-23 15:54:00 +0100

    changed class name

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,30 @@ let pokemonRepository = (function () {
     return pokemonList;
   }
 
-  function addListItem(pokemon) {
-    let listItem = document.createElement('li');
-    let button = document.createElement('button');
+  function showModal(title, text) {
+    const name = document.querySelector("#bootstrapModalLabel");
+    const height = document.querySelector("#modal-height");
+    name.innerHTML = "";
+    height.innerHTML = "";
+    name.innerHTML = title;
+    height.innerHTML = `Height: ${text}`;
+  }
 
+  function addListItem(pokemon) {
+    let ulItem = document.querySelector(".pokemon-list");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+
+    button.setAttribute("data-toggle", "modal");
+    button.setAttribute("data-target", "#bootstrapModal");
     button.innerText = pokemon.name;
-    button.classList.add('custom-button');
+
+    button.classList.add("btn-primary");
+    listItem.classList.add("list-group-item");
+    listItem.appendChild(button);
+    ulItem.appendChild(listItem);
+
+
     button.addEventListener('click', function () {
       showDetails(pokemon);
     });
